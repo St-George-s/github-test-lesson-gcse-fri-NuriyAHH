3b51c8b51196f560cf0b41e543b0a9872d662917
NuriyAHH <afz001@stge.org.uk>
2024-09-02 14:49:25 +0000

    here is my lesson one loops work

---
+++ Task1 Loops.py
@@ -0,0 +1,13 @@
+#Loop to print my name 10 times 
+for i in range(10):
+    print("Nuriya")
+   
+    #string of code that asks for my name and then repeats it 1000 times
+name = input("What is your name? ")
+for i in range(1000):
+    print(name)
+
+    #code that asks for my name and then repeats it with hello 1000 times
+name = input("What is your name? ")
+for i in range(1000):
+    print("Hello, " + name)
---
+++ loops.py
@@ -0,0 +1,4 @@
+#code that asks for my name and then repeats it with hello 1000 times
+name = input("What is your name? ")
+for i in range(1000):
+    print("Hello, " + name)
